717b266f9f9e9e76c4942fc4e08b5f1970e63695
Jay Goyal <jaygoyal@Jay-Goyals-MacBook-Pro.local>
2013-11-16 12:04:55 -0500

    firehouse_weekend; built rails application; splash pag

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehoseNew::Application.config.secret_key_base = 'b75e1e0b6dcd73db0acac59e958376963bb9fc8ae3fa2d18b2e1f9be8ed259be8aa9f3ca812d5cc0e967d81d7c11fe81c69cd81ad12cc5d569187a30cc16fc88'
